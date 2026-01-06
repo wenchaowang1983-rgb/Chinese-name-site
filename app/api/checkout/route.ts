bbed5fa976c83d5754e5464e94caa162dd227a26
Wenchao Wang <wenchao@example.com>
2026-01-06 13:15:36 +1100

    fix stripe apiVersion type error

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,8 +1,6 @@
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST() {
   const session = await stripe.checkout.sessions.create({
